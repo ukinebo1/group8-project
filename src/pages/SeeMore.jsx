3a6939b1040a3e33f4fa865910e7b25698c34b45
Ukinebo1 <perfectprecious2022@gmail.com>
2024-06-13 03:18:12 +0100

    Created the SeeMore page

---
+++ src/pages/SeeMore.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FoodDisplay from '../Components/FoodDisplay/FoodDisplay'
+
+function SeeMore() {
+  return (
+    <div>
+       <FoodDisplay /> 
+    </div>
+  )
+}
+
+export default SeeMore
